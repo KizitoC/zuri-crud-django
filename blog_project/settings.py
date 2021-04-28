f10a806fb04c6f1c6b740dced8049da20b02fe42
Kizito <kizitodamixofoegbu@gmail.com>
2021-04-28 03:11:30 +0100

    Zuri Blog

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 import environ
 
 env = environ.Env()
 environ.Env.read_env()
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
