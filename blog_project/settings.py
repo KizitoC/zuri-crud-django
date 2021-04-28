c5b4433fbe55e4ffa928c9ecdd682f27ff583270
Kizito <kizitodamixofoegbu@gmail.com>
2021-04-28 02:26:58 +0100

    Zuri Blog

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -32,7 +32,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -136,5 +135,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
